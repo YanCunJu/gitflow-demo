88ed989d30efdc826a7f0792229f3c00b1c376c8
yancunju <yancunju@autouc.com>
2022-07-25 16:08:04 +0800

    initial demo

---
+++ main.go
@@ -0,0 +1,14 @@
+/*
+ * @Author: yancunju
+ * @Date: 2022-07-25 16:02:44
+ * @LastEditors: yancunju
+ * @LastEditTime: 2022-07-25 16:03:12
+ * @Description: 请填
+ */
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("callmainfunction")
+}
