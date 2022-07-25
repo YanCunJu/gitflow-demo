6952cfa7269b07ecd67bbdb2cded7fb754c71b13
yancunju <yancunju@autouc.com>
2022-07-25 16:26:37 +0800

    print hello world

--- main.go
+++ main.go
@@ -2,7 +2,7 @@
  * @Author: yancunju
  * @Date: 2022-07-25 16:02:44
  * @LastEditors: yancunju
- * @LastEditTime: 2022-07-25 16:22:47
+ * @LastEditTime: 2022-07-25 16:26:12
  * @Description: 请填
  */
 package main
@@ -11,5 +11,5 @@ import "fmt"
 
 func main() {
 	fmt.Println("call main function")
-	fmt.Println("Hello")
+	fmt.Println("Hello world")
 }
